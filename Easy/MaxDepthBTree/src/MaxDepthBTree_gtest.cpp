f55829b0d04f6110fbd606a8e123e27e012bb700
Alexander Paniutin <alexander.paniutin@gmail.com>
2016-02-20 08:44:51 -0800

    Added tests to MaxDepthBTree

--- Easy/MaxDepthBTree/src/MaxDepthBTree_gtest.cpp
+++ Easy/MaxDepthBTree/src/MaxDepthBTree_gtest.cpp
@@ -13,3 +13,63 @@ TEST(MaxDepthBTree, Empty)
 
     EXPECT_EQ(0,  solution.maxDepth(head));
 }
+
+TEST(MaxDepthBTree, OnlyRoot)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+
+    EXPECT_EQ(1,  solution.maxDepth(head));
+}
+
+TEST(MaxDepthBTree, OneLeftChild)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+    head->left = new TreeNode(2);
+
+    EXPECT_EQ(2,  solution.maxDepth(head));
+}
+
+TEST(MaxDepthBTree, OneRightChild)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+    head->right = new TreeNode(3);
+
+    EXPECT_EQ(2,  solution.maxDepth(head));
+}
+
+TEST(MaxDepthBTree, OneLeftOneRight)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+    head->left = new TreeNode(2);
+    head->right = new TreeNode(3);
+
+    EXPECT_EQ(2,  solution.maxDepth(head));
+}
+
+TEST(MaxDepthBTree, DeepLeftBranch)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+    head->left = new TreeNode(2);
+    head->left->left = new TreeNode(4);
+    head->left->left->left = new TreeNode(5);
+    head->left->left->left->left = new TreeNode(6);
+
+    EXPECT_EQ(5,  solution.maxDepth(head));
+}
+
+TEST(MaxDepthBTree, DeepRightBranch)
+{
+    MaxDepthBTree solution;
+    TreeNode *head = new TreeNode(1);
+    head->right = new TreeNode(2);
+    head->right->right = new TreeNode(4);
+    head->right->right->right = new TreeNode(5);
+    head->right->right->right->right = new TreeNode(6);
+
+    EXPECT_EQ(5,  solution.maxDepth(head));
+}
