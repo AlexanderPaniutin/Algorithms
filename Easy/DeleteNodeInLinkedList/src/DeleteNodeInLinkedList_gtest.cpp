2f7b4d9a935d01fc6fcb0bd09ded673bdef7c367
Alexander Paniutin <alexander.paniutin@gmail.com>
2016-02-20 16:30:07 -0800

    Added tests to DeleteNodeInLinkedList

--- Easy/DeleteNodeInLinkedList/src/DeleteNodeInLinkedList_gtest.cpp
+++ Easy/DeleteNodeInLinkedList/src/DeleteNodeInLinkedList_gtest.cpp
@@ -16,3 +16,51 @@ TEST(DeleteNodeInLinkedList, EmptyList)
     solution.deleteNode(head);
     EXPECT_EQ(NULL, head);
 }
+
+// as per requirement - we should not remove the tail
+TEST(DeleteNodeInLinkedList, RemoveTheOnlyItem)
+{
+    DeleteNodeInLinkedList solution;
+    ListNode *head = new ListNode(1);
+
+    solution.deleteNode(head);
+    EXPECT_EQ(1, head->val);
+    EXPECT_EQ(NULL, head->next);
+}
+
+TEST(DeleteNodeInLinkedList, RemoveHead)
+{
+    DeleteNodeInLinkedList solution;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+
+    solution.deleteNode(head);
+    EXPECT_EQ(2, head->val);
+    EXPECT_EQ(NULL, head->next);
+}
+
+// as per requirement - we should not remove the tail
+TEST(DeleteNodeInLinkedList, RemoveTail)
+{
+    DeleteNodeInLinkedList solution;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+
+    solution.deleteNode(head->next);
+    EXPECT_EQ(1, head->val);
+    EXPECT_EQ(2, head->next->val);
+    EXPECT_EQ(NULL, head->next->next);
+}
+
+TEST(DeleteNodeInLinkedList, RemoveMiddle)
+{
+    DeleteNodeInLinkedList solution;
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+
+    solution.deleteNode(head->next);
+    EXPECT_EQ(1, head->val);
+    EXPECT_EQ(3, head->next->val);
+    EXPECT_EQ(NULL, head->next->next);
+}
